18ff49d83d153e0486d55c5584f6e13304f6f81e
NoOne <muhammadbilaltts41@gmail.com>
2025-09-15 14:06:18 +0500

    Added a cutomer details DTO

---
+++ Poc.Infrastructure/DTOs/Customer/CustomerDetailDTO.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Poc.Common.RemoteValidation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Poc.Infrastructure.DTOs.Customer
+{
+    public class CustomerDetailDTO
+    {
+        public Guid UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(320)]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        public string Phone { get; set; }
+
+        [StringLength(15)]
+        public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "Country is Required")]
+        [StringLength(50)]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "State is Required")]
+        [StringLength(200)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "City is Required")]
+        [StringLength(200)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Address is Required")]
+        public string Address { get; set; }
+
+
+
+        //for customers business details
+        [Required(ErrorMessage = "Business Name is Required")]
+        [StringLength(100)]
+        public string BusinessType { get; set; }
+
+        [Required(ErrorMessage = "Country is Required")]
+        [StringLength(50)]
+        public string BusinessCountry { get; set; }
+
+        [Required(ErrorMessage = "State is Required")]
+        [StringLength(200)]
+        public string BusinessState { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(200)]
+        public string BusinessCity { get; set; }
+
+        [Required(ErrorMessage = "Address is Required")]
+        public string BusinessAddress { get; set; }
+
+
+        //for customer prof of business details
+
+
+        public string RegistrationFileContent { get; set; }
+
+
+        public string MemorandumFileContent { get; set; }
+
+
+        public string LicenseFileContent { get; set; }
+
+        public string TaxFileContent { get; set; }
+
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Type { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime? CreatedDate { get; set; }
+
+        public bool Status { get; set; }
+
+        public Guid ModifiedBy { get; set; }
+
+    }
+}
